a22184ca9b18c076edbb917a9f55080cba7a0266
xiaopi <271107942@qq.com>
2018-10-07 17:12:26 +0800

    添加基本input组件样式

--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import Button from './button'
 import Icon from  './Icon'
 import ButtonGroup from './button-group'
+import GInput from './ginput'
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
 Vue.component('g-button-group', ButtonGroup)
+Vue.component('g-input', GInput)
 
 new Vue({
     el: '#app',
@@ -16,102 +18,102 @@ new Vue({
     }
 })
 
-//单元测试pwd
-import chai from 'chai'
-import spies from 'chai-spies'
-chai.use(spies)
-
-const expect = chai.expect
-try {
-
-
-    {
-        let Constructor = Vue.extend(Button)
-        const vm = new Constructor({
-            propsData:{
-                icon: 'settings'
-            }
-        })
-        vm.$mount()
-        let useElement = vm.$el.querySelector('use')
-        console.log(useElement.getAttribute('xlink:href'))
-        expect(useElement.getAttribute('xlink:href')).to.eq('#i-settings')
-        vm.$el.remove()
-        vm.$destroy()
-
-
-    }
-    {
-        let Constructor = Vue.extend(Button)
-        const vm = new Constructor({
-            propsData:{
-                icon: 'settings',
-                loading: true
-            }
-        })
-        vm.$mount()
-        const  useElement = vm.$el.querySelector('use')
-        const href = useElement.getAttribute('xlink:href')
-        expect(href).to.eq('#i-loading')
-        vm.$el.remove()
-        vm.$destroy()
-
-    }
-    {
-        const div = document.createElement('div')
-        document.body.appendChild(div)
-        let Constructor = Vue.extend(Button)
-        const vm = new Constructor({
-            propsData:{
-                icon: 'settings'
-            }
-        })
-        vm.$mount(div)
-        const  svg = vm.$el.querySelector('svg')
-        let {order} = window.getComputedStyle(svg)
-        expect(order).to.eq("1")
-        vm.$el.remove()
-        vm.$destroy()
-
-    }
-    {
-        const div = document.createElement('div')
-        document.body.appendChild(div)
-        let Constructor = Vue.extend(Button)
-        const vm = new Constructor({
-            propsData:{
-                icon: 'settings',
-                iconPosition: 'right'
-            }
-        })
-        vm.$mount(div)
-        const  svg = vm.$el.querySelector('svg')
-        let {order} = window.getComputedStyle(svg)
-        expect(order).to.eq("2")
-        vm.$el.remove()
-        vm.$destroy()
-    }
-    {
-        //mock 安装 npm -D i chai-spies
-        let Constructor = Vue.extend(Button)
-        const vm = new Constructor({
-            propsData:{
-                icon: 'settings',
-                loading: true
-            }
-        })
-        vm.$mount()
-        const spy = chai.spy(function(){})
-        vm.$on('click', spy)
-        //希望这个函数被执行
-        let button = vm.$el
-        button.click()
-        expect(spy).to.have.been.called()
-    }
-} catch (e) {
-    window.errors = [e]
-} finally {
-    window.errors && window.errors.forEach((e)=>{
-        console.error(e.message)
-    })
-}
+// //单元测试pwd
+// import chai from 'chai'
+// import spies from 'chai-spies'
+// chai.use(spies)
+//
+// const expect = chai.expect
+// try {
+//
+//
+//     {
+//         let Constructor = Vue.extend(Button)
+//         const vm = new Constructor({
+//             propsData:{
+//                 icon: 'settings'
+//             }
+//         })
+//         vm.$mount()
+//         let useElement = vm.$el.querySelector('use')
+//         console.log(useElement.getAttribute('xlink:href'))
+//         expect(useElement.getAttribute('xlink:href')).to.eq('#i-settings')
+//         vm.$el.remove()
+//         vm.$destroy()
+//
+//
+//     }
+//     {
+//         let Constructor = Vue.extend(Button)
+//         const vm = new Constructor({
+//             propsData:{
+//                 icon: 'settings',
+//                 loading: true
+//             }
+//         })
+//         vm.$mount()
+//         const  useElement = vm.$el.querySelector('use')
+//         const href = useElement.getAttribute('xlink:href')
+//         expect(href).to.eq('#i-loading')
+//         vm.$el.remove()
+//         vm.$destroy()
+//
+//     }
+//     {
+//         const div = document.createElement('div')
+//         document.body.appendChild(div)
+//         let Constructor = Vue.extend(Button)
+//         const vm = new Constructor({
+//             propsData:{
+//                 icon: 'settings'
+//             }
+//         })
+//         vm.$mount(div)
+//         const  svg = vm.$el.querySelector('svg')
+//         let {order} = window.getComputedStyle(svg)
+//         expect(order).to.eq("1")
+//         vm.$el.remove()
+//         vm.$destroy()
+//
+//     }
+//     {
+//         const div = document.createElement('div')
+//         document.body.appendChild(div)
+//         let Constructor = Vue.extend(Button)
+//         const vm = new Constructor({
+//             propsData:{
+//                 icon: 'settings',
+//                 iconPosition: 'right'
+//             }
+//         })
+//         vm.$mount(div)
+//         const  svg = vm.$el.querySelector('svg')
+//         let {order} = window.getComputedStyle(svg)
+//         expect(order).to.eq("2")
+//         vm.$el.remove()
+//         vm.$destroy()
+//     }
+//     {
+//         //mock 安装 npm -D i chai-spies
+//         let Constructor = Vue.extend(Button)
+//         const vm = new Constructor({
+//             propsData:{
+//                 icon: 'settings',
+//                 loading: true
+//             }
+//         })
+//         vm.$mount()
+//         const spy = chai.spy(function(){})
+//         vm.$on('click', spy)
+//         //希望这个函数被执行
+//         let button = vm.$el
+//         button.click()
+//         expect(spy).to.have.been.called()
+//     }
+// } catch (e) {
+//     window.errors = [e]
+// } finally {
+//     window.errors && window.errors.forEach((e)=>{
+//         console.error(e.message)
+//     })
+// }
---
+++ src/ginput.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="wrapper" :class="{error}">
+        <input :value="value1" type="text" :disabled="disabled"
+            :readonly="readonly" />
+        <template v-if="error">
+            <g-icon icon="error" class="icon-error"></g-icon>
+            <span class="errorMessage">{{error}}</span>
+        </template>
+
+    </div>
+</template>
+
+<script>
+    import GIcon from './Icon'
+
+    export default {
+        name: "ginput",
+        components: {GIcon},
+        props: {
+            value1: {
+                type: String
+            },
+            disabled: {
+                type:Boolean,
+                default: false
+            },
+            readonly: {
+                type: Boolean,
+                default: false
+            },
+            error: {
+                type: String
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    $height: 32px;
+    $border-color: #999;
+    $border-color-hover: #666;
+    $font-size: 12px;
+    $box-shadow-color: rgba(0,0,0,0.5);
+    $red: #F14530;
+    .wrapper {
+        font-size: $font-size ;display: inline-flex;align-items: center;
+        :not(last-child){margin-right: 0.5em;}
+        &.error { > input {border-color: $red;} }
+        .icon-error{fill: $red;}
+        .errorMessage{color: $red;}
+        > input {height: $height;border: 1px solid $border-color;border-radius: 4px;padding: 0 8px;font-size: inherit;
+            &:hover{border-color: $border-color-hover;}
+            &:focus{outline: none;box-shadow: inset 0 1px 3px $box-shadow-color;
+            }
+            &[disabled], &[readonly] {border-color: #bbb;color: #bbb;cursor: not-allowed;}
+        }
+
+    }
+
+</style>
