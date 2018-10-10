a069477e9c980e92ee9c02f051211f9aa4d827a6
xiaopi <271107942@qq.com>
2018-10-10 19:12:54 +0800

    重构grid

--- src/col.vue
+++ src/col.vue
@@ -1,5 +1,6 @@
 <template>
-    <div class="col" :class="`col-${span}`">
+    <div class="col" :class="colSpan"
+         :style="colPadding">
         <slot></slot>
     </div>
 </template>
@@ -8,7 +9,26 @@
     export default {
         name: "g-col",
         props: {
-            span: [Number, String]
+            span: [Number, String],
+            offset: [Number, String]
+        },
+        data(){
+            return {
+                gutter: 0
+            }
+        },
+        computed: {
+            colSpan(){
+                let {span, offset} = this
+                return [span &&`col-${span}`, offset && `offset-${offset}`]
+            },
+            colPadding(){
+                let {gutter} = this
+                return {
+                    paddingLeft: gutter/2 +'px',
+                    paddingRight: gutter/2 + 'px'
+                }
+            }
         }
     }
 </script>
@@ -16,14 +36,22 @@
 <style lang="scss" scoped>
     .col{
         height: 100px;
-        background: grey;
+        /*background: grey;*/
         width: 50%;
-        border: 1px solid red;
+        /*border: 1px solid red;*/
+        padding: 0 10px;
         $class-prefix: col-;
         @for $n from 1 through 24 {
             &.#{$class-prefix}#{$n} {
                 width: ($n / 24) * 100%;
             }
         }
+        $class-prefix: offset-;
+        @for $n from 1 through 24 {
+            &.#{$class-prefix}#{$n} {
+                margin-left: ($n / 24) * 100%
+            }
+        }
+
     }
 </style>
--- src/row.vue
+++ src/row.vue
@@ -1,12 +1,26 @@
 <template>
-    <div class="row">
+    <div class="row" :style="rolStyle">
         <slot></slot>
     </div>
 </template>
 
 <script>
     export default {
-        name: "g-row"
+        name: "g-row",
+        props:{
+            gutter: [Number, String]
+        },
+        computed:{
+          rolStyle(){
+              const {gutter} = this
+              return {marginLeft: -gutter/2+'px',marginRight:-gutter/2+'px'}
+          }
+        },
+        mounted(){
+            this.$children.forEach(vm=>{
+                vm.gutter = this.gutter
+            })
+        }
     }
 </script>
 
