68173d144050d6a5ddb28e56df9e1614a2194dfc
xiaopi <271107942@qq.com>
2018-10-21 11:58:44 +0800

    fix window.scoll bug

--- src/popover.vue
+++ src/popover.vue
@@ -1,9 +1,11 @@
 <template>
     <div class="popover" @click.stop="xxx">
-        <div class="content-wrapper" v-if="visible" @click.stop>
+        <div ref="contentWrapper" class="content-wrapper" v-if="visible" @click.stop>
             <slot name="content" ></slot>
         </div>
+        <span ref="triggerWrapper">
         <slot></slot>
+        </span>
     </div>
 </template>
 
@@ -20,6 +22,11 @@
                 this.visible = !this.visible
                 if (this.visible === true) {
                     this.$nextTick(()=>{
+                       document.body.appendChild(this.$refs.contentWrapper)
+                        let {width, height, top, left} = this.$refs.triggerWrapper.getBoundingClientRect()
+                        this.$refs.contentWrapper.style.left = `${window.scrollX+top}px`
+
+                        this.$refs.contentWrapper.style.top = `${window.scrollY+left}px`
                        let eventHandler = ()=> {
                            this.visible = false
                            document.removeEventListener('click', eventHandler)
@@ -38,12 +45,13 @@
         display: inline-flex;
         vertical-align: top;
         position: relative;
-        > .content-wrapper{
+
+    }
+    .content-wrapper{
             position: absolute;
-            bottom:100%;
-            left: 0;
             border: 1px solid red;
             box-shadow:  0 0 3px rgba(0,0,0, 0.5);
+            transform: translateY(-100%)
+
         }
-    }
 </style>
